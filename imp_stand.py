f59e349dea22c4264c149ccdaff1772f075c25ee
Isaac Etungu <isaacetungu@gmail.com>
2026-04-01 18:16:45 +0300

    Update imp_stand.py

--- imp_stand.py
+++ imp_stand.py
@@ -87,7 +87,7 @@ def process_files(qn_bank, survey_files, manager_name):
     ]
 
     survey_resp = survey_resp.loc[
-        :, survey_resp.columns.astype(str).str.fullmatch(r"\d+")
+        :, survey_resp.columns.astype(str).str.fullmatch(r"\d{4}")
     ]
 
     reshape = survey_resp.melt(var_name="question", value_name="response")
