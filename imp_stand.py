7efa0dc110fc1fa877d4bf004c41e45b2b60d8ea
Isaac Etungu <isaacetungu@gmail.com>
2026-04-01 16:59:54 +0300

    Update imp_stand.py

--- imp_stand.py
+++ imp_stand.py
@@ -111,18 +111,26 @@ def process_files(qn_bank, survey_files, manager_name):
     df.loc[mask, "response"] = pd.to_numeric(df.loc[mask, "response"], errors="coerce")
 
     final = df.pivot(index="record_id", columns="qno_q_group", values="response")
-
-    num_cols = df.loc[mask, 'qno_q_group'].unique()
-    final[num_cols] = final[num_cols].apply(pd.to_numeric, errors='coerce')
-    final = final.dropna(how='all').reset_index(drop=True)
+    
+    final = final.reset_index(drop=True)
     final.columns.name = None
+    final = final.apply(pd.to_numeric, errors="ignore")
+    
     year_series = pd.to_datetime(final.get("1106_survey_date_completion"), errors="coerce").dt.year
+    
     year_mode = year_series.mode()
+    
     final["1003_survey_year"] = int(year_mode.iloc[0]) if not year_mode.empty else None
+    
     final["1104_survey_person_manager"] = manager_name
+    
     first_cols = final.columns[:4]
+    
     final = final.dropna(subset=first_cols).reset_index(drop=True)
+    final.columns = final.columns.str.strip().str.replace("\u200b", "", regex=True)
+    
     st.write(final)
+    
     return final
     
     # final.columns.name = None
