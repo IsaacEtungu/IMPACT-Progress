4701501e06af29f3ff2c19faf6228146bf838e18
Isaac Etungu <isaacetungu@gmail.com>
2026-04-01 18:31:28 +0300

    Update imp_stand.py

--- imp_stand.py
+++ imp_stand.py
@@ -92,7 +92,7 @@ def process_files(qn_bank, survey_files, manager_name):
             suffixes=("", "_dup")
         )
 
-survey_resp = survey_resp.loc[:, ~survey_resp.columns.str.endswith("_dup")]
+    survey_resp = survey_resp.loc[:, ~survey_resp.columns.str.endswith("_dup")]
 
     survey_resp = survey_resp.loc[:, ~survey_resp.columns.str.endswith("_dup")]
 
